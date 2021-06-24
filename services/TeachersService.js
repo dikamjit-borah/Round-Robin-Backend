a09aecd4d91b633fd6f735fe3788bdc10f1cf26e
Dikamjit Borah <learningdjb99@gmail.com>
2021-06-24 21:05:49 +0530

    apis

--- services/TeachersService.js
+++ services/TeachersService.js
@@ -19,9 +19,66 @@ exports.fetch_week_view = async (teacher_id, week_start_date, week_end_date, res
 
   exports.insert_into_schedules = async (user_inputs, res ) =>{
 
+    console.log(user_inputs["scheduled_date"]);
+    console.log("For new schedule, ", user_inputs);
+
+    
+
+    const q1 = `SELECT scheduled_start_time, scheduled_end_time from schedules WHERE scheduled_date = ?`
+
+    db_connect.query(q1, [user_inputs[4]], (err, result) => {
+      if(err)
+        throw err;
+      else{
+        //console.log( );
+        if(checkIfValidTime(result, user_inputs[5], user_inputs[6]))
+        {
+          insertIntoDb(user_inputs, res)
+          
+        }
+        else
+        {
+          res.json({status:200, isValidStart:false})
+        }
+      }
+    })
+  }
+
+  function insertIntoDb(user_inputs, res)
+  {
     const q = `INSERT into schedules (schedule_id, teacher_id, teacher_name, scheduled_topic, scheduled_date, scheduled_start_time, scheduled_end_time) VALUES (?,?,?,?,?,?,?);`;
   
-    await db_connect.query(q, user_inputs)
-    return true;
+    db_connect.query(q, user_inputs, (err, result)=>{
+      if(err)
+        res.json({status:500, message:err})
+      else{
+        res.json({status:200, isValidStart:true})
+      }
+    })
 
+  }
+
+  function checkIfValidTime(arrayOfTimes, scheduledStart, scheduledEnd)
+  {
+    let scheduledStart24 = convertTo24HrInt(scheduledStart)
+    let scheduledEnd24 = convertTo24HrInt(scheduledEnd)
+    for(let i = 0; i<arrayOfTimes.length; i++)
+    {
+      let start24 = convertTo24HrInt(arrayOfTimes[i]["scheduled_start_time"])
+      let end24 = convertTo24HrInt(arrayOfTimes[i]["scheduled_end_time"])
+
+      if(scheduledStart24>=start24 && scheduledStart24<=end24)
+      {
+        console.log("Did not insert because of time overlap");
+        return false;
+      }
+     
+    }
+
+    return true
+  }
+
+  function convertTo24HrInt(hrsStr)
+  {
+    return hrsStr.replace(":", "").split(":")[0]
   }
